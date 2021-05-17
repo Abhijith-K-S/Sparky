b53181b072c49923ae1041a478cd0c05bec073c4
Abhijith-K-S <abhijithks1729@gmail.com>
2021-05-17 12:12:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sparky/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.sparky
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
+        Handler().postDelayed({
+            ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+        },1500)
+        Handler().postDelayed({
+            ic_logo.visibility = View.GONE
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },1500)
+    }
+}
